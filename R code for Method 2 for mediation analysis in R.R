e4db4da8d6af420264c972130f0f6c6bc876820e
William <104100408+TidyPython@users.noreply.github.com>
2022-08-09 09:26:07 -0400

    Add files via upload

---
+++ R code for Method 2 for mediation analysis in R.R
@@ -0,0 +1,60 @@
+# Method 2 for mediation analysis in R
+
+# read data
+Mediation_data <- 
+  read.csv('https://raw.githubusercontent.com/TidyPython/Mediation_analysis/main/mediation_hypothetical_data.csv')
+
+# show the head of the data
+head(Mediation_data)
+
+# Mediation Analysis in R from Scratch 
+
+library(boot)
+set.seed(123)
+
+
+Mediation_function<-function(data_used,i)
+{
+  # Sample a data 
+  data_temp=data_used[i,]
+  # a path 
+  result_a<-lm(M~X, data = data_temp)
+  a<-result_a$coefficients[2]
+  
+  # b path
+  result_b<-lm(Y~M+X, data = data_temp)
+  b<-result_b$coefficients[2]
+  
+  #calculating the indirect effect
+  indirect_effect<-a*b
+  return(indirect_effect)
+}
+
+
+# use boot() to do bootstrapping mediation analysis
+boot_mediation <- boot(Mediation_data, Mediation_function, R=5000)
+boot_mediation 
+
+# print out confidence intervals for indirect effect
+boot.ci(boot.out = boot_mediation, type = c("norm", "perc"))
+
+# plot the 5000 indirect effects
+plot(boot_mediation)
+
+# print out top 6 indirect effects saved in "boot_mediation"
+head(boot_mediation$t)
+
+# calculate the standard deviation
+sd(boot_mediation$t)
+# print out the bootstrapping output again
+boot_mediation
+
+#The difference between the mean of the bootstrap estimates
+# and the original sample estimate is the bias.
+bias=mean(boot_mediation$t)-boot_mediation$t0
+print(bias)
+
+# calculate the confidence interval from scratch
+print(boot_mediation$t0-bias + c(-1, 1) * 1.96 * sd(boot_mediation$t))
+# Again, print out confidence interval based on normal distribution
+boot.ci(boot.out = boot_mediation, type = c("norm"))
